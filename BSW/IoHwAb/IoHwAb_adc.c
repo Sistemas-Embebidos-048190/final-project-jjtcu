da5e907ff34a7e28de92925336882c1bb5dc261b
Daniel Diaz <daniel_dp96@hotmail.com>
2025-11-26 14:46:59 -0600

    ADC0 B14 fixed by Profesor Platon

--- BSW/IoHwAb/IoHwAb_adc.c
+++ BSW/IoHwAb/IoHwAb_adc.c
@@ -1,7 +1,10 @@
 
 #include "IoHwAb_adc.h"
+#include "fsl_vref.h"
+#include "fsl_spc.h"
 
 #define TCM_LPADC_BASE              ADC0
+#define DEMO_VREF_BASE VREF0
 
 // IDs de comandos LPADC (1–15 válidos)
 #define TCM_LPADC_CMDID_OUTPUT      1U   // P0_14  Output_Speed_Sensor
@@ -24,6 +27,14 @@
 // Y déjalo ASÍ:
 #define TCM_LPADC_RESULT_MASK   (0xFFFFU)
 #define TCM_LPADC_RESULTSHIFT   (0U)
+
+
+#define DEMO_LPADC_VREF_SOURCE           kLPADC_ReferenceVoltageAlt3
+#define DEMO_LPADC_DO_OFFSET_CALIBRATION true
+#define DEMO_LPADC_OFFSET_VALUE_A        0x10U
+#define DEMO_LPADC_OFFSET_VALUE_B        0x10U
+
+#define DEMO_SPC_BASE           SPC0
 
 
 void Init_ADC_Pins(void)
@@ -53,13 +64,23 @@ void Init_ADC_Pins(void)
 
 void TCM_LPADC_InitSensors(void)
 {
+
+	vref_config_t vrefConfig;
 	/* 1) Configurar el reloj funcional de ADC0 */
 	    CLOCK_SetClkDiv(kCLOCK_DivAdc0Clk, 1U);      // divisor = 1
 	    CLOCK_AttachClk(kFRO_HF_to_ADC0);            // FRO_HF (48 MHz) → ADC0
 
 	    /* 2) Habilitar clock de periférico y reset */
-	    CLOCK_EnableClock(kCLOCK_Adc0);
-	    RESET_PeripheralReset(kADC0_RST_SHIFT_RSTn);
+	    //CLOCK_EnableClock(kCLOCK_Adc0);
+	    //RESET_PeripheralReset(kADC0_RST_SHIFT_RSTn);
+
+	    /* enable VREF */
+	        SPC_EnableActiveModeAnalogModules(DEMO_SPC_BASE, kSPC_controlVref);
+
+	        VREF_GetDefaultConfig(&vrefConfig);
+	        vrefConfig.bufferMode = kVREF_ModeBandgapOnly;
+	        /* Initialize VREF module, the VREF module is only used to supply the bias current for LPADC. */
+	        VREF_Init(DEMO_VREF_BASE, &vrefConfig);
 
 	    lpadc_config_t              lpadcConfig;
 	    lpadc_conv_command_config_t cmdConfig;
@@ -67,76 +88,83 @@ void TCM_LPADC_InitSensors(void)
 
 	    /* Config básica del LPADC */
 	    LPADC_GetDefaultConfig(&lpadcConfig);
+	    lpadcConfig.powerLevelMode = kLPADC_PowerLevelAlt4;
 	    lpadcConfig.enableAnalogPreliminary = true;
-	    lpadcConfig.powerUpDelay            = 0x10U;
-	#if defined(FSL_FEATURE_LPADC_HAS_CFG_PWRSEL) && FSL_FEATURE_LPADC_HAS_CFG_PWRSEL
-	    lpadcConfig.powerLevelMode          = kLPADC_PowerLevelAlt4;
-	#endif
+	    lpadcConfig.referenceVoltageSource = 2U;
+	    //lpadcConfig.enableAnalogPreliminary = true;
+	    //lpadcConfig.powerUpDelay            = 0x10U;
+//	#if defined(FSL_FEATURE_LPADC_HAS_CFG_PWRSEL) && FSL_FEATURE_LPADC_HAS_CFG_PWRSEL
+//	    //lpadcConfig.powerLevelMode          = kLPADC_PowerLevelAlt4;
+//	#endif
+//
+//	    /* (Opcional pero recomendable: usar misma referencia que el demo) */
+//	#if defined(DEMO_LPADC_VREF_SOURCE)
+//	    lpadcConfig.referenceVoltageSource  = DEMO_LPADC_VREF_SOURCE;
+//	#endif
 
-	    /* (Opcional pero recomendable: usar misma referencia que el demo) */
-	#if defined(DEMO_LPADC_VREF_SOURCE)
-	    lpadcConfig.referenceVoltageSource  = DEMO_LPADC_VREF_SOURCE;
-	#endif
 
+
+
+
+
+	lpadcConfig.conversionAverageMode = kLPADC_ConversionAverage128;
 	LPADC_Init(TCM_LPADC_BASE, &lpadcConfig);
 
+	LPADC_DoOffsetCalibration(TCM_LPADC_BASE);
+
+	LPADC_DoAutoCalibration(TCM_LPADC_BASE);
 
 
-    /* Limpiar FIFO y banderas */
-#if (defined(FSL_FEATURE_LPADC_FIFO_COUNT) && (FSL_FEATURE_LPADC_FIFO_COUNT == 2U))
-    LPADC_DoResetFIFO0(TCM_LPADC_BASE);
-    LPADC_DoResetFIFO1(TCM_LPADC_BASE);
-#else
-    LPADC_DoResetFIFO(TCM_LPADC_BASE);
-#endif
-    LPADC_ClearStatusFlags(TCM_LPADC_BASE,
-                           kLPADC_ResultFIFO0OverflowFlag |
-                           kLPADC_ResultFIFO0ReadyFlag);
-
-    /* Si quieres más precisión, puedes habilitar calibración:
-       LPADC_DoAutoCalibration(TCM_LPADC_BASE);
-    */
-
-    /* Config base de comandos de conversión */
+//    /* Limpiar FIFO y banderas */
+//#if (defined(FSL_FEATURE_LPADC_FIFO_COUNT) && (FSL_FEATURE_LPADC_FIFO_COUNT == 2U))
+//    LPADC_DoResetFIFO0(TCM_LPADC_BASE);
+//    LPADC_DoResetFIFO1(TCM_LPADC_BASE);
+//#else
+//    LPADC_DoResetFIFO(TCM_LPADC_BASE);
+//#endif
+//    LPADC_ClearStatusFlags(TCM_LPADC_BASE,
+//                           kLPADC_ResultFIFO0OverflowFlag |
+//                           kLPADC_ResultFIFO0ReadyFlag);
+//
+//    /* Si quieres más precisión, puedes habilitar calibración:
+//       LPADC_DoAutoCalibration(TCM_LPADC_BASE);
+//    */
+//
+//    /* Config base de comandos de conversión */
     LPADC_GetDefaultConvCommandConfig(&cmdConfig);
-    cmdConfig.hardwareAverageMode = kLPADC_HardwareAverageCount4;
-    cmdConfig.sampleTimeMode      = kLPADC_SampleTimeADCK3;
-    cmdConfig.loopCount           = 0U;
-    cmdConfig.hardwareCompareMode = kLPADC_HardwareCompareDisabled;
-#if defined(FSL_FEATURE_LPADC_HAS_CMDL_MODE) && FSL_FEATURE_LPADC_HAS_CMDL_MODE
-    cmdConfig.conversionResolutionMode = kLPADC_ConversionResolutionHigh; // 16 bits
-#endif
+
 
     /* OUTPUT_SPEED_SENSOR  (P0_14 = ADC0_B14 → canal 14, lado B) */
     cmdConfig.channelNumber     = 14U;
     cmdConfig.sampleChannelMode = kLPADC_SampleChannelSingleEndSideB;
     LPADC_SetConvCommandConfig(TCM_LPADC_BASE, TCM_LPADC_CMDID_OUTPUT, &cmdConfig);
 
-    /* FLUID_TRANS_TEMPERATURE (P0_22 = ADC0_A14 → canal 14, lado A) */
-    cmdConfig.channelNumber     = 14U;
-    cmdConfig.sampleChannelMode = kLPADC_SampleChannelSingleEndSideA;
-    LPADC_SetConvCommandConfig(TCM_LPADC_BASE, TCM_LPADC_CMDID_FLUID, &cmdConfig);
-
-    /* TURBINE_SPEED_SENSOR (P0_15 = ADC0_B15 → canal 15, lado B) */
-    cmdConfig.channelNumber     = 15U;
-    cmdConfig.sampleChannelMode = kLPADC_SampleChannelSingleEndSideB;
-    LPADC_SetConvCommandConfig(TCM_LPADC_BASE, TCM_LPADC_CMDID_TURBINE, &cmdConfig);
+//    /* FLUID_TRANS_TEMPERATURE (P0_22 = ADC0_A14 → canal 14, lado A) */
+//    cmdConfig.channelNumber     = 14U;
+//    cmdConfig.sampleChannelMode = kLPADC_SampleChannelSingleEndSideA;
+//    LPADC_SetConvCommandConfig(TCM_LPADC_BASE, TCM_LPADC_CMDID_FLUID, &cmdConfig);
+//
+//    /* TURBINE_SPEED_SENSOR (P0_15 = ADC0_B15 → canal 15, lado B) */
+//    cmdConfig.channelNumber     = 15U;
+//    cmdConfig.sampleChannelMode = kLPADC_SampleChannelSingleEndSideB;
+//    LPADC_SetConvCommandConfig(TCM_LPADC_BASE, TCM_LPADC_CMDID_TURBINE, &cmdConfig);
 
     /* Triggers de software para cada comando */
     LPADC_GetDefaultConvTriggerConfig(&trigConfig);
+    trigConfig.targetCommandId = TCM_LPADC_CMDID_OUTPUT;
     trigConfig.enableHardwareTrigger = false;  // puro software trigger
 
-    trigConfig.targetCommandId = TCM_LPADC_CMDID_OUTPUT;
+    //trigConfig.targetCommandId = TCM_LPADC_CMDID_OUTPUT;
     LPADC_SetConvTriggerConfig(TCM_LPADC_BASE, TCM_LPADC_TRIG_OUTPUT, &trigConfig);
-
-    trigConfig.targetCommandId = TCM_LPADC_CMDID_FLUID;
-    LPADC_SetConvTriggerConfig(TCM_LPADC_BASE, TCM_LPADC_TRIG_FLUID, &trigConfig);
-
-    trigConfig.targetCommandId = TCM_LPADC_CMDID_TURBINE;
-    LPADC_SetConvTriggerConfig(TCM_LPADC_BASE, TCM_LPADC_TRIG_TURBINE, &trigConfig);
+//
+//    trigConfig.targetCommandId = TCM_LPADC_CMDID_FLUID;
+//    LPADC_SetConvTriggerConfig(TCM_LPADC_BASE, TCM_LPADC_TRIG_FLUID, &trigConfig);
+//
+//    trigConfig.targetCommandId = TCM_LPADC_CMDID_TURBINE;
+//    LPADC_SetConvTriggerConfig(TCM_LPADC_BASE, TCM_LPADC_TRIG_TURBINE, &trigConfig);
 
     /* Encender el ADC (igual que en muchos ejemplos) */
-    LPADC_Enable(TCM_LPADC_BASE, true);
+    //LPADC_Enable(TCM_LPADC_BASE, true);
 }
 
 
@@ -144,26 +172,21 @@ void TCM_LPADC_InitSensors(void)
 
 
 
-
-uint16 TCM_LPADC_ReadByTrigger(uint32 triggerIndex)
-{
-    lpadc_conv_result_t result;
-    uint32 triggerMask = (1UL << triggerIndex);
-
-    LPADC_DoSoftwareTrigger(TCM_LPADC_BASE, triggerMask);
-
-    while (!LPADC_GetConvResult(TCM_LPADC_BASE, &result, 0U))
-    {
-    }
-
-    return (uint16)result.convValue;  // 0..65535
-}
 
 
 
 void TCM_Read_OutputSpeedSensorRaw(void)
 {
-	Write_TCM_OutputSpeed_OSS( TCM_LPADC_ReadByTrigger(TCM_LPADC_TRIG_OUTPUT) );
+	lpadc_conv_result_t result;
+	uint32 triggerMask = (1UL << 0);
+	const uint32_t g_LpadcResultShift = 3U;
+
+	LPADC_DoSoftwareTrigger(TCM_LPADC_BASE, triggerMask);
+
+	while (!LPADC_GetConvResult(TCM_LPADC_BASE, &result, 0U))
+	{
+	}
+	Write_TCM_OutputSpeed_OSS( (result.convValue) >> g_LpadcResultShift  );
 }
 
 void TCM_Read_FluidTempSensorRaw(void)
